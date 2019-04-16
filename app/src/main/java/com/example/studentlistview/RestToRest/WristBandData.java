86fd21b6c0e1c825f047c6d9377b8c10bfd7521c
MortenMC-easj <31882401+MortenMC-easj@users.noreply.github.com>
2019-04-16 13:43:09 +0200

    Har lavet en klasse KUN til den autogeneret data som Wristband API'en spytter ud og bruger den i den nyligt tilføjede ConsumeWristbandData.

--- app/src/main/java/com/example/studentlistview/RestToRest/WristBandData.java
+++ app/src/main/java/com/example/studentlistview/RestToRest/WristBandData.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.example.studentlistview.R;
 import com.example.studentlistview.REST.Data;
+import com.google.gson.annotations.SerializedName;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,118 +26,103 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WristBandData extends AppCompatActivity {
-    public static final String BASE_URI = "https://berthawristbandrestprovider.azurewebsites.net/api/wristbanddata";
-    public static final String LOG_TAG = "simple_rest";
+public class WristBandData {
+
+    private int deviceId;
+    private Double pm25;
+    private Double pm10;
+    private int co2;
+    private int o3;
+    private Double pressure;
+    private Double temperature;
+    private Double humidity;
+
+    public WristBandData(int deviceId, Double pm25, Double pm10, int co2, int o3, Double pressure, Double temperature, Double humidity) {
+        this.deviceId = deviceId;
+        this.pm25 = pm25;
+        this.pm10 = pm10;
+        this.co2 = co2;
+        this.o3 = o3;
+        this.pressure = pressure;
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public int getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(int deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public Double getPm25() {
+        return pm25;
+    }
+
+    public void setPm25(Double pm25) {
+        this.pm25 = pm25;
+    }
+
+    public Double getPm10() {
+        return pm10;
+    }
+
+    public void setPm10(Double pm10) {
+        this.pm10 = pm10;
+    }
+
+    public int getCo2() {
+        return co2;
+    }
+
+    public void setCo2(int co2) {
+        this.co2 = co2;
+    }
+
+    public int getO3() {
+        return o3;
+    }
+
+    public void setO3(int o3) {
+        this.o3 = o3;
+    }
+
+    public Double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(Double pressure) {
+        this.pressure = pressure;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //String single_url = BASE_URI + "/1";
-        /*try {
-            String jsonString = readJSonFeed(single_url);
-        } catch (IOException e) {
-           Log.e(LOG_TAG, e.getMessage());
-        }*/
-
-        final ReadJSONFeedTask task = new ReadJSONFeedTask();
-        task.execute(BASE_URI);
+    public String toString() {
+        return "WristBandData{" +
+                "deviceId=" + deviceId +
+                ", pm25=" + pm25 +
+                ", pm10=" + pm10 +
+                ", co2=" + co2 +
+                ", o3=" + o3 +
+                ", pressure=" + pressure +
+                ", temperature=" + temperature +
+                ", humidity=" + humidity +
+                '}';
     }
-
-
-    private class ReadJSONFeedTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            try {
-                return readJSonFeed(urls[0]);
-            } catch (IOException ex) {
-                Log.e(LOG_TAG, ex.toString());
-                cancel(true);
-                return ex.toString();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String jsonString) {
-            final TextView textView = findViewById(R.id.mainResultTextView);
-            Log.d(LOG_TAG, jsonString);
-            try {
-                List<Data> datas = new ArrayList<>();
-                JSONArray array = new JSONArray(jsonString);
-                //JSONObject jsonObject = new JSONObject(jsonString);
-                for (int i = 0; i < array.length(); i++) {
-                    JSONObject obj = array.getJSONObject(i);
-                    final int deviceId = obj.getInt("deviceId");
-                    final double pm25 = obj.getDouble("pm25");
-                    final double pm10 = obj.getDouble("pm10");
-                    final int co2 = obj.getInt("co2");
-                    final int o3 = obj.getInt("o3");
-                    final double pressure = obj.getDouble("pressure");
-                    final double temperature = obj.getDouble("temperature");
-                    final double humidity = obj.getDouble("humidity");
-                    final String userId = obj.getString("userId");
-                    Data data = new Data(deviceId, pm25, pm10, co2, o3, pressure, temperature, humidity, userId);
-                    datas.add(data);
-                    //textView.setText(title + ": " + body + "\n");
-                }
-                ArrayAdapter<Data> adapter =
-                        new ArrayAdapter<Data>(getBaseContext(), android.R.layout.simple_list_item_1, datas);
-                ListView listView = findViewById(R.id.mainListView);
-                listView.setAdapter(adapter);
-
-            } catch (JSONException ex) {
-                textView.append(ex.toString());
-            }
-        }
-
-        @Override
-        protected void onCancelled(String message) {
-            super.onCancelled(message);
-            final TextView textView = findViewById(R.id.mainResultTextView);
-            textView.setText(message);
-        }
-    }
-
-
-    private String readJSonFeed(String urlString) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-        final InputStream content = openHttpConnection(urlString);
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-        while (true) {
-            final String line = reader.readLine();
-            if (line == null)
-                break;
-            stringBuilder.append(line);
-        }
-        return stringBuilder.toString();
-    }
-
-    private InputStream openHttpConnection(final String urlString) throws IOException {
-        final URL url = new URL(urlString);
-        final URLConnection conn = url.openConnection();
-        if (!(conn instanceof HttpURLConnection))
-            throw new IOException("Not an HTTP connection");
-        final HttpURLConnection httpConn = (HttpURLConnection) conn;
-        httpConn.setAllowUserInteraction(false);
-        // No user interaction like dialog boxes, etc.
-        httpConn.setInstanceFollowRedirects(true);
-        // follow redirects, response code 3xx
-        httpConn.setRequestMethod("GET");
-        httpConn.connect();
-        final int response = httpConn.getResponseCode();
-        if (response == HttpURLConnection.HTTP_OK) {
-            return httpConn.getInputStream();
-        } else {
-            throw new IOException("HTTP response not OK");
-        }
-    }
-
-
-
-
-
-
 }
