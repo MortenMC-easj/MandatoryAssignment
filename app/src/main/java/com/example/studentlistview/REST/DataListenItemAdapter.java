f3d0d69320c42a8f086c487f231c456efb123214
MortenMC-easj <31882401+MortenMC-easj@users.noreply.github.com>
2019-04-16 14:28:28 +0200

    Tilføjet ekstra data til DataListViewet, så man kan se mere information når man logger ind

--- app/src/main/java/com/example/studentlistview/REST/DataListenItemAdapter.java
+++ app/src/main/java/com/example/studentlistview/REST/DataListenItemAdapter.java
@@ -32,6 +32,13 @@ public class DataListenItemAdapter extends ArrayAdapter<Data> {
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         Data data = getItem(position);
         Integer deviceId = data.getDeviceId();
+        Double pm25 = data.getPm25();
+        Double pm10 = data.getPm10();
+        Integer co2 = data.getCo2();
+        Integer o3 = data.getO3();
+        Double pressure = data.getPressure();
+        Double temperature = data.getTemperature();
+        Double humidity = data.getHumidity();
         String userId = data.getUserId();
         LinearLayout dataView;
         if (convertView == null) {
@@ -43,9 +50,23 @@ public class DataListenItemAdapter extends ArrayAdapter<Data> {
             dataView = (LinearLayout) convertView;
         }
         TextView deviceIdView = dataView.findViewById(R.id.datalist_item_deviceId);
+        TextView pm25View = dataView.findViewById(R.id.datalist_item_pm25);
+        TextView pm10View = dataView.findViewById(R.id.datalist_item_pm10);
+        TextView co2View = dataView.findViewById(R.id.datalist_item_co2);
+        TextView o3View = dataView.findViewById(R.id.datalist_item_o3);
+        TextView pressureView = dataView.findViewById(R.id.datalist_item_pressure);
+        TextView temperatureView = dataView.findViewById(R.id.datalist_item_temperature);
+        TextView humidityView = dataView.findViewById(R.id.datalist_item_humidity);
         TextView userIdView = dataView.findViewById(R.id.datalist_item_userId);
-        deviceIdView.setText("Device: " + Integer.toString(deviceId));
-        userIdView.setText(" User: " + userId);
+        deviceIdView.setText(" Device: " + Integer.toString(deviceId));
+        pm25View.setText(" Pm25: " + Double.toString(pm25));
+        pm10View.setText(" Pm10: " + Double.toString(pm10));
+        co2View.setText(" Co2: " + Integer.toString(co2));
+        o3View.setText(" o3: " + Integer.toString(o3));
+        pressureView.setText(" Pressure: " + Double.toString(pressure));
+        temperatureView.setText(" Temperature: " + Double.toString(temperature));
+        humidityView.setText(" Humidity: " + Double.toString(humidity));
+        userIdView.setText("User: " + userId);
         return dataView;
     }
 
